4cbeda2c51bf38da51cd77aff4a9da217aa19cab
stefvanesch <stefvanesch@users.noreply.github.com>
2020-10-10 15:14:18 +0000

    Fix styling

--- src/Size.php
+++ src/Size.php
@@ -7,11 +7,13 @@ use Marshmallow\HelperFunctions\Facades\Size as SizeFacade;
 
 class Size extends Number
 {
-	protected $to = 'Auto';
-	protected $from = 'bytes';
-	protected $precision = 2;
+    protected $to = 'Auto';
 
-	public function __construct($name, $attribute = null, $resolveCallback = null)
+    protected $from = 'bytes';
+
+    protected $precision = 2;
+
+    public function __construct($name, $attribute = null, $resolveCallback = null)
     {
         parent::__construct($name, $attribute, $resolveCallback);
         $this->help('The value shown in this field is a size in <strong>Bytes</strong/>.');
@@ -20,33 +22,36 @@ class Size extends Number
 
     public function isStoredAs($from)
     {
-    	$this->from = $from;
-    	$this->help('The value shown in this field is a size in <strong>'. $from .'</strong/>.');
-    	$this->setResolveUsing();
-    	return $this;
+        $this->from = $from;
+        $this->help('The value shown in this field is a size in <strong>'. $from .'</strong/>.');
+        $this->setResolveUsing();
+
+        return $this;
     }
 
     public function displayAs($to)
     {
-    	$this->to = $to;
-    	$this->setResolveUsing();
-    	return $this;
+        $this->to = $to;
+        $this->setResolveUsing();
+
+        return $this;
     }
 
     public function precision($precision)
     {
-    	$this->precision = $precision;
-    	$this->setResolveUsing();
-    	return $this;
+        $this->precision = $precision;
+        $this->setResolveUsing();
+
+        return $this;
     }
 
     protected function setResolveUsing()
     {
-    	$this->displayUsing(function ($value, $model, $column) {
-        	return SizeFacade::of($value)
-		        				->from($this->from)
-		        				->to($this->to)
-		        				->convert($this->precision);
+        $this->displayUsing(function ($value, $model, $column) {
+            return SizeFacade::of($value)
+                                ->from($this->from)
+                                ->to($this->to)
+                                ->convert($this->precision);
         });
     }
 }
